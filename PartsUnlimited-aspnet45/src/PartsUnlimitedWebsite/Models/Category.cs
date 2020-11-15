910d56a3fcd9d7251edde0e21505b128ec73ebea
yaminisatishkumar <yaminisatishkumar@outlook.com>
2020-11-15 12:17:05 +0000

    Category change. Fixes #1

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
@@ -3,6 +3,8 @@ using System.ComponentModel.DataAnnotations;
 
 namespace PartsUnlimited.Models
 {
+
+    // My third change
     public class Category
     {
         public int CategoryId { get; set; }
